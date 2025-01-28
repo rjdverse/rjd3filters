77eebcaae2ab747fa3bec0ef8a687d53b55488ad
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2025-01-28 15:02:07 +0100

    some indentation

--- R/zzz.R
+++ R/zzz.R
@@ -5,26 +5,25 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
-  if (!requireNamespace("rjd3toolkit", quietly = TRUE)) stop("Loading rjd3 libraries failed")
-  # For debugts_ging: to see if Jars are effectively loaded
-  # options(java.parameters = "-verbose:class")
+    if (!requireNamespace("rjd3toolkit", quietly = TRUE)) stop("Loading rjd3 libraries failed")
+    # For debugts_ging: to see if Jars are effectively loaded
+    # options(java.parameters = "-verbose:class")
 
-  # TODO : devtools will look only in RJDemetra3\java for JAR files so copied them there too
-  result <- .jpackage(pkgname, lib.loc=libname)
-  if (!result) stop("Loading java packages failed")
+    # TODO : devtools will look only in RJDemetra3\java for JAR files so copied them there too
+    result <- .jpackage(pkgname, lib.loc=libname)
+    if (!result) stop("Loading java packages failed")
 
-  # what's your java  version?  Need > 1.5.0.
-  jversion <- .jcall('java.lang.System','S','getProperty','java.version')
-  if (jversion < "1.8.0") {
-    stop(paste("Your java version is ", jversion,
-               ".  Need 1.8.0 or higher.", sep=""))
-  }
+    # what's your java  version?  Need > 1.5.0.
+    jversion <- .jcall('java.lang.System','S','getProperty','java.version')
+    if (jversion < "1.8.0") {
+        stop("Your java version is ", jversion, ".  Need 1.8.0 or higher.")
+    }
 
 }
 
 .onAttach <- function(libname , pkgname) {
-  jversion <- .jcall('java.lang.System','S','getProperty','java.version')
-  packageStartupMessage("Java requirements fullfilled, found version ",jversion)
+    jversion <- .jcall('java.lang.System','S','getProperty','java.version')
+    packageStartupMessage("Java requirements fullfilled, found version ",jversion)
 }
 
 #' Seasonally Adjusted Retail Sales
